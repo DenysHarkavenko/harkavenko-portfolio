b66c5f306504420dc118271cb335a29cfd43eae5
Denys Harkavenko <garkavenkodenispost@gmail.com>
2023-12-21 15:41:41 +0100

    decomposition

--- src/components/Main/SkillsList/SkillList.tsx
+++ src/components/Main/SkillsList/SkillList.tsx
@@ -1,71 +1,11 @@
 import './SkillList.css'
-const npm: string = require('../../../images/npm.png')
-const sql: string = require('../../../images/MySQL.png')
-const axure: string = require('../../../images/axure.png')
-const figma: string = require("../../../images/figma.png")
+import { Icon } from '@iconify/react';
+import { frontEndSkills } from '../../../data/skills';
+import { backEndSkills } from '../../../data/skills';
+import { knowledge } from '../../../data/skills';
+import { languages } from '../../../data/skills';
+import { softSkills } from '../../../data/skills';
 
-const knowledge: string[] = ['Responsive markup', 'JavaScript', 'React', 'REST API', 'Relation databases']
-const languages: string[] = ['Ukrainian (native)', 'English (A2)', 'Russian (native)', 'Czech (A1)']
-
-interface ISkills {
-    title: string,
-    icon: JSX.Element | string,
-}
-
-const frontEndSkills:ISkills[] = [
-    {
-        title: "HTML5",
-        icon: <i className="bx bxl-html5"style={{ color: 'purple' }}></i>,
-    },
-    {
-        title: "CSS3",
-        icon: <i className="bx bxl-css3" style={{ color: 'blue' }}></i>,
-    },
-    {
-        title: "Saas",
-        icon:  <i className="bx bxl-sass" style={{ color: 'palevioletred' }}></i>,
-    },
-    {
-        title: "Bootstrap",
-        icon: <i className="bx bxl-bootstrap"style={{ color: 'purple' }}></i>,
-    },
-    {
-        title: "Tailwind",
-        icon: <i className="bx bxl-tailwind-css" style={{ color: 'teal' }}></i>,
-    },
-    {
-        title: "JavaScript",
-        icon:  <i className="bx bxl-javascript" style={{ color: 'orange' }}></i>,
-    },
-    {
-        title: "TypeScript",
-        icon:  <i className="bx bxl-typescript" style={{ color: 'blue' }}></i>,
-    },
-    {
-        title: "React",
-        icon:  <i className="bx bxl-react"></i>,
-    }
-
-]
-
-const backEndSkills:ISkills[] =[
-    {
-        title: "Node.js",
-        icon: <i className="bx bxl-nodejs" style={{ color: 'green' }}></i>,
-    },
-    {
-        title: "Express",
-        icon: <i className="bx bxl-nodejs" style={{ color: 'green' }}></i>,
-    },
-    {
-        title: "npm",
-        icon: <img className='npm' src={npm} alt="Not Found" />,
-    },
-    {
-        title: "MySQL",
-        icon: <img className='sql' src={sql} alt="Not Found" />,
-    }
-]
 
 export default function Skills() {
     return (
@@ -73,8 +13,8 @@ export default function Skills() {
         <div className="main__skills">
             {/* Unmark list */}
             <h1>Industry Knowledge</h1>
-            <ul>
-                {knowledge.map(val => <li>{val}</li>)}
+            <ul className="unmark__list">
+                {knowledge.map(val => <li className="unmark__list">{val}</li>)}
             </ul>
             <br />
             {/* First Mark list */}
@@ -92,55 +32,53 @@ export default function Skills() {
             <h1>Other Technical Skills</h1>
             <h2>Programming languages:</h2>
             <p>
-                <i className="bx bxl-c-plus-plus" style={{ color: 'blue' }}></i>C++, 
-                C#, 
-                <i className="bx bxl-python"></i> Python (basic).
+                <Icon icon="devicon:cplusplus" width="20" height="20" className="icon" />C++, 
+                <Icon icon="devicon:csharp" width="20" height="20" className="icon" />C#, 
+                <Icon icon="logos:python" width="20" height="20" className="icon" /> Python (basic).
             </p>
             {/* Text line 2 */}
             <h2>Technologies:</h2>
             <p>
-                <i className="bx bxl-git" style={{color: "red"}}></i> Git,
-                <i className="bx bxl-github" style={{color: "gray"}}></i> GitHub,
-                <img src={figma} alt="Not Found"/> Figma, 
-                <img src={axure} alt="Not Found"/> Axure,
-                <i className="bx bxl-adobe" style={{color: "#061148"}}></i> Adobe PS.
+                <Icon icon="devicon:git" width="20" height="20" className="icon" /> Git,
+                <Icon icon="akar-icons:github-fill" width="20" height="20" className="icon" /> GitHub,
+                <Icon icon="devicon:figma" height="20" className="icon" /> Figma, 
+                <Icon icon="devicon:photoshop" width="20" height="20" className="icon" /> Adobe PS.
             </p>
             <br />
             {/* Text line 3 */}
             <h1>Soft-skills</h1>
             <p>
-                Mathematical critical thinking, disciplined, responsible,
-                positive, sociable, humane, polite !
+                {softSkills.map(val => val + "")}
             </p>
             <br />
-            {/* Second Mark List  */}
+            {/* Unmark List */}
             <h1>Languages</h1>
-            <ul>
-                {languages.map(val => <li>{val}</li>)}
+            <ul className="unmark__list">
+                {languages.map(val => <li className="unmark__list">{val}</li>)}
             </ul>
             <br />
-            {/* Third Matk List */}
+            {/* Third Mark List */}
             <h1>Media</h1>
-            <ul>
-                <li>
+            <ul className="unmark__list">
+                <li className="unmark__list">
                     <a
                         href="https://www.linkedin.com/in/denys-harkavenko-52234a251/"
                         target="_blank"
                     >
-                        <i className="bx bxl-linkedin-square"></i> LinkedIn
+                        <Icon icon="devicon:linkedin" width="20" height="20" className="icon" /> LinkedIn
                     </a>
                 </li>
-                <li>
+                <li className="unmark__list">
                     <a
                         href="https://github.com/DenysHarkavenko"
                         target="_blank"
                     >
-                        <i className="bx bxl-github"></i> GitHub
+                        <Icon icon="akar-icons:github-fill" width="20" height="20" className="icon" /> GitHub
                     </a>
                 </li>
-                <li>
+                <li className="unmark__list">
                     <a href="#" target="_blank">
-                        <i className="bx bxl-telegram"></i> Telegram
+                    <Icon icon="bi:telegram" width="20" height="20" style={{color:"skyblue"}} className="icon"  /> Telegram
                     </a>
                 </li>
             </ul>
---
+++ src/data/skills.tsx
@@ -0,0 +1,73 @@
+import { Icon } from '@iconify/react';
+
+export const knowledge: string[] = ['Responsive markup', 'JavaScript', 'React', 'REST API', 'Relation databases']
+export const languages: string[] = ['Ukrainian (native),', 'English (A2),', 'Russian (native),', 'Czech (A1).']
+export const softSkills: string[] = ['Mathematical critical thinking,', ' disciplined,', ' responsible,', ' positive,', ' sociable,', ' humane,', ' polite !']
+
+interface ISkills {
+    title: string,
+    icon: JSX.Element | string,
+    iconComponent?: typeof Icon,
+}
+
+export const frontEndSkills:ISkills[] = [
+    {
+        title: "HTML5",
+        icon: <Icon icon="vscode-icons:file-type-html" width="20" height="20" className="icon" />,
+    },
+    {
+        title: "CSS3",
+        icon: <Icon icon="vscode-icons:file-type-css" width="20" height="20" className="icon" />,
+        // icon: <i className="bx bxl-css3" style={{ color: 'blue' }}></i>,
+    },
+    {
+        title: "Saas",
+        icon:  <Icon icon="logos:sass" width="20" height="20" className="icon" />,
+        // icon:  <i className="bx bxl-sass" style={{ color: 'palevioletred' }}></i>,
+    },
+    {
+        title: "Bootstrap",
+        icon: <Icon icon="logos:bootstrap" width="20" height="20" className="icon" />,
+        // icon: <i className="bx bxl-bootstrap"style={{ color: 'purple' }}></i>,
+    },
+    {
+        title: "Tailwind",
+        icon: <Icon icon="vscode-icons:file-type-tailwind" width="20" height="20" className="icon" />,
+        // icon: <i className="bx bxl-tailwind-css" style={{ color: 'teal' }}></i>,
+    },
+    {
+        title: "JavaScript",
+        icon:  <Icon icon="logos:javascript" width="20" height="20" className="icon" />,
+        // icon:  <i className="bx bxl-javascript" style={{ color: 'orange' }}></i>,
+    },
+    {
+        title: "TypeScript",
+        icon:  <Icon icon="devicon:typescript" width="20" height="20" className="icon" />,
+        // icon:  <i className="bx bxl-typescript" style={{ color: 'blue' }}></i>,
+    },
+    {
+        title: "React",
+        icon:  <Icon icon="skill-icons:react-dark" width="20" height="20" className="icon" />,
+        // icon:  <i className="bx bxl-react"></i>,
+    }
+
+]
+
+export const backEndSkills:ISkills[] =[
+    {
+        title: "Node.js",
+        icon: <Icon icon="vscode-icons:file-type-node" width="20" height="20" className="icon" />,
+    },
+    {
+        title: "Express",
+        icon: <Icon icon="vscode-icons:file-type-node" width="20" height="20" className="icon" />,
+    },
+    {
+        title: "npm",
+        icon: <Icon icon="mdi:npm" height="24" style={{color: "red"}} className="icon" />,
+    },
+    {
+        title: "MySQL",
+        icon: <Icon icon="devicon:mysql" height="24" className="icon" />,
+    }
+]
